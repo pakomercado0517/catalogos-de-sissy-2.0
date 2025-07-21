2e8e214b1a9151c3e1c2036be7f8dd04c49c058a
pakomercado0517 <sissyyayle0517@hotmail.com>
2025-07-21 15:30:12 -0600

    fix skeleton components

--- src/components/SissyCatalogues.jsx
+++ src/components/SissyCatalogues.jsx
@@ -79,6 +79,12 @@ export default function SissyCatalogues() {
           <div>
             <CardSkeleton className="animate-fade-up" />
           </div>
+          <div>
+            <CardSkeleton className="animate-fade-up" />
+          </div>
+          <div>
+            <CardSkeleton className="animate-fade-up" />
+          </div>
         </>
         // <div className="w-full my-48 flex md:justify-center md:items-center lg:justify-items-center lg:self-center ">
         //   <Modal show={handleModal}>
