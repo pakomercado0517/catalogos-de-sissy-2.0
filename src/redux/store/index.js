3d4dd0113f2ae464a8c4cb46f12ddd046668269c
pakomercado0517 <sissyyayle0517@hotmail.com>
2026-07-13 18:57:36 -0600

    Update project dependencies and configurations: Upgrade React to version 19.2.7, update ESLint settings for React 19, modify jsconfig.json for bundler module resolution, and adjust Tailwind CSS configuration for flowbite-react. Refactor Redux store to improve middleware handling and enhance component lifecycle management in ChatBox and CompaniesList components.

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import rootReducer from "../reducer";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import rootReducer from "../reducer";
 
 const persistConfig = {
   key: "catsissy",
@@ -15,8 +22,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: [thunk],
-  devTools: composeWithDevTools(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
